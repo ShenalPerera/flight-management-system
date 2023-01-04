ce3bbdad20243f7e8f96dab49798ae5ec4979297
thisaramanohara <thisaramanohara@gmail.com>
2023-01-04 12:19:12 +0000

    Values can be got from the form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,26 +14,34 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule} from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { DetailsComponent } from './routes-screen/details/details.component';
+import { FormComponent } from './routes-screen/form/form.component';
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     RoutesScreenComponent,
     FaresScreenComponent,
-    DetailsComponent
+    DetailsComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
+    // NoopAnimationsModule,
     MatTableModule,
     MatAutocompleteModule,
     FormsModule,
     MatFormFieldModule,
     ReactiveFormsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/routes-screen/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+  }
+}
