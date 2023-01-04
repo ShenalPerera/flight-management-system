8ec83a9cae05c66c51d6d1a4354a95464901c85e
thisaramanohara <thisaramanohara@gmail.com>
2023-01-04 04:41:30 +0000

    Routes hardcoded

--- frontend/src/app/routes-screen/routes-screen.component.ts
+++ frontend/src/app/routes-screen/routes-screen.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./routes-screen.component.scss']
 })
 export class RoutesScreenComponent {
+  routes = [
+    {
+      routeID: 1,
+      departure: "Sri Lanka",
+      destination: "Dubai",
+      mileage: 1223.45,
+      durationH: 12,
+      durationM: 30
+    },
+    {
+      routeID: 1,
+      departure: "Sri Lanka",
+      destination: "Dubai",
+      mileage: 1223.45,
+      durationH: 12,
+      durationM: 30
+    },
+    {
+      routeID: 1,
+      departure: "Sri Lanka",
+      destination: "Dubai",
+      mileage: 1223.45,
+      durationH: 12,
+      durationM: 30
+    },
+    {
+      routeID: 1,
+      departure: "Sri Lanka",
+      destination: "Dubai",
+      mileage: 1223.45,
+      durationH: 12,
+      durationM: 30
+    },
+    {
+      routeID: 1,
+      departure: "Sri Lanka",
+      destination: "Dubai",
+      mileage: 1223.45,
+      durationH: 12,
+      durationM: 30
+    }
+  ]
 
 }
