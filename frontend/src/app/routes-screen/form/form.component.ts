0c111e60369dc8fb54fbbb9c105eb59b49aa4ca3
thisaramanohara <thisaramanohara@gmail.com>
2023-01-05 09:27:52 +0000

    Edit works

--- frontend/src/app/routes-screen/form/form.component.ts
+++ frontend/src/app/routes-screen/form/form.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {NgForm} from "@angular/forms";
 
+import {ALL_ROUTES} from "../shared/routes";
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -11,6 +13,7 @@ export class FormComponent implements OnChanges{
   // @Input() formData: [number, string, string, number, number, number] = [0, '', '', 0, 0, 0];
   @Input() formData: [number, string, string, number, number, number] = [NaN, '', '', NaN, NaN, NaN];
 
+  routeID!: number;
   departure!: string;
   destination!: string;
   mileage!: number;
@@ -20,6 +23,12 @@ export class FormComponent implements OnChanges{
   onSubmit(f: NgForm) {
     console.log(f.value);  // { first: '', last: '' }
     console.log(f.value['departure']);  // { first: '', last: '' }
+    const found = ALL_ROUTES.find((obj)=>{
+      if (this.routeID == obj.routeID) {
+        // console.log("has to be changed from "+obj.departure+" to "+f.value['departure']);
+        obj.departure = f.value['departure'];
+      }
+    })
   }
 
   constructor() {
@@ -31,6 +40,7 @@ export class FormComponent implements OnChanges{
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
     if (this.formData != undefined) {
+      this.routeID = this.formData[0];
       this.departure = this.formData[1];
       this.destination = this.formData[2];
       this.mileage = this.formData[3];
