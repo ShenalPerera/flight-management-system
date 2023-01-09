17ba8a66547aa9d8029d7c07426ffa8a623abc2f
pubuducb <pubuducb@gmail.com>
2023-01-09 09:49:14 +0000

    Implemented routes for all end-points

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaresScreenComponent } from "./fares-screen/fares-screen.component";
+import { RoutesScreenComponent } from "./routes-screen/routes-screen.component";
+import { FlightsComponent } from "./flights/flights.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: FaresScreenComponent },
+  { path: "routes", component: RoutesScreenComponent },
+  { path: "flights", component: FlightsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
