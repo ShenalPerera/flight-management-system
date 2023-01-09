6d5742ae2d64ef2673a78b65f8c65d01269eec79
Shenal Perera <shenalperera00@gmail.com>
2023-01-09 18:54:31 +0000

    Changed the flight data model -  ad id filed

--- frontend/src/app/flights/flight.model.ts
+++ frontend/src/app/flights/flight.model.ts
@@ -1,6 +1,6 @@
 export class Flight{
 
-
+  public id: number | undefined;
   public flight_number: number | undefined;
   public arrival: string | undefined;
   public departure: string | undefined;
@@ -11,7 +11,7 @@ export class Flight{
 
   public departure_time:string | undefined;
 
-  constructor(flight_number:number, arrival:string, departure:string,arrival_date:string, departure_date:string,
+  constructor(id:number,flight_number:number, arrival:string, departure:string,arrival_date:string, departure_date:string,
               arrival_time:string, departure_time:string){
     this.flight_number = flight_number;
     this.arrival = arrival;
@@ -20,6 +20,7 @@ export class Flight{
     this.departure_date = departure_date;
     this.arrival_time = arrival_time;
     this.departure_time =departure_time;
+    this.id = id
   }
 
 
--- frontend/src/assets/data-service.ts
+++ frontend/src/assets/data-service.ts
@@ -7,11 +7,11 @@ import {Injectable} from "@angular/core";
 
 export class DataService{
   flights : Flight[] = [
-    new Flight(1,"colombo","India","2023-01-04","2023-01-05", "13:04","11:00"),
+    new Flight(1,1,"colombo","India","2023-01-04","2023-01-05", "13:04","11:00"),
 
-    new Flight(2,"Singapore","Sri Lanka","2023-01-01","2023-01-08","09:11","10:23"),
-    new Flight(3,"Dubai","India","2023-02-03","2023-01-01","12:14","12:30"),
-    new Flight(3,"Dubaiwe","sfIndia","2023-01-01","2023-01-01","08:00","08:30"),
+    new Flight(2,2,"Singapore","Sri Lanka","2023-01-01","2023-01-08","09:11","10:23"),
+    new Flight(3,3,"Dubai","India","2023-02-03","2023-01-01","12:14","12:30"),
+    new Flight(4,3,"Dubaiwe","sfIndia","2023-01-01","2023-01-01","08:00","08:30"),
 
 
   ];
@@ -24,7 +24,17 @@ export class DataService{
     this.flights.push(new_flight);
   }
 
-  removeFlight(flight : Flight){
+  removeFlight(flight_id : number){
+    const flightIndex = this.flights.findIndex((flightElement :Flight) => {
+      return flightElement.id === flight_id;
+    });
+
+    if (flightIndex !== -1) {
+      this.flights.splice(flightIndex, 1);
+    }
+  }
+
+  updateFlight(flight:Flight){
     const flightIndex = this.flights.findIndex((flightElement :Flight) => {
       return flightElement.flight_number === flight.flight_number &&
         flightElement.departure === flight.departure &&
@@ -33,8 +43,7 @@ export class DataService{
         flightElement.departure_date === flight.departure_date
     });
 
-    if (flightIndex !== -1) {
-      this.flights.splice(flightIndex, 1);
-    }
+
   }
+
 }
