5a034229e6b87b0ca7deca13c63985c72a651ccc
pubuducb <pubuducb@gmail.com>
2023-01-10 18:43:23 +0000

    Changed metadata on the website

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,54 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule} from "@angular/material/table";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSelectModule } from "@angular/material/select";
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { MatExpansionModule } from "@angular/material/expansion";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoutesScreenComponent } from './routes-screen/routes-screen.component';
 import { FaresScreenComponent } from './fares-screen/fares-screen.component';
-import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatTableModule} from "@angular/material/table";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FlightsComponent } from './flights/flights.component';
-import {HeaderComponent} from "./header/header.component";
+import { HeaderComponent } from "./header/header.component";
 import { FlightItemComponent } from './flights/flight-item/flight-item.component';
 import { DataFilterPipe } from './data-filter.pipe';
-import {MatInputModule} from "@angular/material/input";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
 import { FormComponent } from './routes-screen/form/form.component';
-import { MatSelectModule } from "@angular/material/select";
 import { FareItemComponent } from './fares-screen/fare-item/fare-item.component';
 import { FareFormComponent } from './fares-screen/fare-form/fare-form.component';
-
-import {RouteService} from "./routes-screen/services/route.service";
-import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import { RouteService } from "./routes-screen/services/route.service";
 import { CreateFormComponent } from './routes-screen/create-form/create-form.component';
-import {MatExpansionModule} from "@angular/material/expansion";
-import {EntryComponent} from "./routes-screen/entry/entry.component";
+import { EntryComponent } from "./routes-screen/entry/entry.component";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        RoutesScreenComponent,
-        FaresScreenComponent,
-        FlightsComponent,
-        HeaderComponent,
-        HeaderComponent,
-        FlightItemComponent,
-        DataFilterPipe,
-        FormComponent,
-        EntryComponent,
-        CreateFormComponent,
-        FareItemComponent,
-        FareFormComponent
-
-    ],
+  declarations: [
+    AppComponent,
+    RoutesScreenComponent,
+    FaresScreenComponent,
+    FlightsComponent,
+    HeaderComponent,
+    HeaderComponent,
+    FlightItemComponent,
+    DataFilterPipe,
+    FormComponent,
+    EntryComponent,
+    CreateFormComponent,
+    FareItemComponent,
+    FareFormComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -67,7 +64,6 @@ import {EntryComponent} from "./routes-screen/entry/entry.component";
     BrowserAnimationsModule,
     MatDialogModule,
     MatExpansionModule
-
   ],
   providers: [
     RouteService,
