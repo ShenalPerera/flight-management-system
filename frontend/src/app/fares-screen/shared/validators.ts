ce862c451ba61b311a2e6561b28777d0ea88607b
pubuducb <pubuducb@gmail.com>
2023-01-13 04:40:09 +0000

    Resolved issues with the validators in 'fare-form'

--- frontend/src/app/fares-screen/shared/validators.ts
+++ frontend/src/app/fares-screen/shared/validators.ts
@@ -2,24 +2,22 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export const locationValidator: ValidatorFn =
   (control: AbstractControl): ValidationErrors | null => {
-    const departure = control.get('departure');
-    const arrival = control.get('arrival');
-    if (
-      (departure?.value.toLowerCase() == arrival?.value.toLowerCase()) &&
-      (departure?.value != '') && (arrival?.value != '')
-    ) {
-      return { sameLocation : true };
-    }
-    return null;
-  };
+  const departure = control.get('departure');
+  const arrival = control.get('arrival');
+  if (
+    (departure?.value.toLowerCase() == arrival?.value.toLowerCase()) &&
+    (departure?.value != '') && (arrival?.value != '')
+  ) {
+    return { sameLocation : true };
+  }
+  return null;
+};
 
 export const numberValidator: ValidatorFn =
   (control: AbstractControl): ValidationErrors | null => {
-    if (control.value == null) {
-      return { notANumber : true };
-    }
-    if (control.value <= 0) {
-      return { notAPositiveNumber : true };
-    }
-    return null;
-  };
+  if (control.value == null)
+    return { notANumber : true };
+  if (control.value < 0)
+    return { notAPositiveNumber : true };
+  return null;
+};
