67b2ce0b849b070823e3afc8dc3797dd3cbcbc0d
thisaramanohara <thisaramanohara@gmail.com>
2023-01-23 11:25:30 +0000

    Added logger to the controller

--- fms/src/main/java/com/fms/routes_screen/routes_controllers/RoutesController.java
+++ fms/src/main/java/com/fms/routes_screen/routes_controllers/RoutesController.java
@@ -2,6 +2,8 @@ package com.fms.routes_screen.routes_controllers;
 
 import com.fms.routes_screen.models.Route;
 import com.fms.routes_screen.services.RoutesService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +19,12 @@ public class RoutesController {
 
 //    @Autowired
     RoutesService routesService;
+    private Logger logger;
 
     @Autowired
     RoutesController(RoutesService routesService) {
         this.routesService = routesService;
+        this.logger = LoggerFactory.getLogger(RoutesController.class);
     }
 
     @GetMapping("/api/routes-screen")
@@ -30,26 +34,33 @@ public class RoutesController {
 
     @GetMapping("/api/routes-screen/get-routes")
     ResponseEntity<List<Route>> sendAllRoutes() {
+        logger.info("'/api/routes-screen/get-routes' accessed");
         return new ResponseEntity<>(routesService.sendAllRoutes(), HttpStatus.OK);
     }
 
     @PostMapping("/api/routes-screen/create-route")
     ResponseEntity<Route> createRoute(@RequestBody Route route) {
+        logger.info("'/api/routes-screen/create-route' accessed with dep->{},des->{},mil->{},hrs->{}",
+                route.getDeparture(), route.getDestination(), route.getMileage(), route.getDurationH());
         return routesService.createRoute(route);
     }
 
     @PutMapping("/api/routes-screen/update-route")
     ResponseEntity<Route> editRoute(@RequestBody Route route) {
+        logger.info("'/api/routes-screen/update-route' accessed with dep->{},des->{},mil->{},hrs->{}",
+                route.getDeparture(), route.getDestination(), route.getMileage(), route.getDurationH());
         return routesService.editRoute(route);
     }
 
     @DeleteMapping("/api/routes-screen/delete-route")
     ResponseEntity<Integer> deleteRoute(@RequestParam int routeID){
+        logger.info("'/api/routes-screen/delete-route' accessed with routeID->{}", routeID);
         return routesService.deleteRoute(routeID);
     }
 
     @GetMapping("/api/routes-screen/search-routes")
     ResponseEntity<List<Route>> searchRoutes(@RequestParam String departure, @RequestParam String destination) {
+        logger.info("'/api/routes-screen/search-routes' accessed with dep->{}, des->{}", departure, destination);
         return routesService.searchRoutes(departure, destination);
     }
 
