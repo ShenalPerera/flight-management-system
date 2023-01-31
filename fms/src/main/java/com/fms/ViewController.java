f7b264f02316b718cda3bee43a64513c7a9b60d7
pubuducb <pubuducb@gmail.com>
2023-01-31 06:16:48 +0000

    Resolved routing issue by implementing 'ViewContoller'

---
+++ fms/src/main/java/com/fms/ViewController.java
@@ -0,0 +1,10 @@
+package com.fms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping({"/fares", "/routes", "/flights"})
+    public String index() { return "forward:/index.html"; }
+}
