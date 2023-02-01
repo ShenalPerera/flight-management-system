6c0138ed21dc4352583777035037fddd3af733f6
Shenal Perera <shenalperera00@gmail.com>
2023-02-01 16:55:54 +0000

    Fixed a bug in the custom query in the JPArepository

--- fms/src/main/java/com/fms/flights/repositories/FlightRepository.java
+++ fms/src/main/java/com/fms/flights/repositories/FlightRepository.java
@@ -14,7 +14,13 @@ public interface FlightRepository extends JpaRepository<Flight, String> {
 
     int countAllByFlightNumberAndDepartureDate(String flightNumber, String departureDate);
 
-    @Query("SELECT CASE WHEN COUNT(f) = 1 AND f.flightId = : flightId THEN TRUE ELSE FALSE END FROM flight f WHERE " +
+    @Query("SELECT " +
+            "   CASE " +
+            "       WHEN COUNT(f) = 1 AND f.flightId = : flightId " +
+            "       THEN TRUE " +
+            "       ELSE FALSE " +
+            "   END " +
+            "FROM flight f WHERE " +
             "f.flightNumber = :flightNumber AND f.departureDate = :departureDate or f.flightId = :flightId")
     boolean isFlightExitsAndNoDuplicatesFound(@Param("flightNumber") String flightNumber,@Param("departureDate") String departureDate,@Param("flightId") Long flightId);
 }
