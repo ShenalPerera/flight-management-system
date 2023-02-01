c9402ac51148c014fa152da86fd70331852dc950
Shenal Perera <shenalperera00@gmail.com>
2023-02-01 18:28:30 +0000

    Added quer to create a table

--- fms/src/main/java/com/fms/flights/createFlightSchema.sql
+++ fms/src/main/java/com/fms/flights/createFlightSchema.sql
@@ -1,4 +1,14 @@
 # Create the table schema
-# CREATE TABLE flight(
-#
-# )
+CREATE TABLE flight(
+    flight_id bigint primary key auto_increment,
+    flight_number char(4) not null ,
+    departure varchar(20) not null,
+    arrival varchar(20) not null ,
+    departure_date date not null ,
+    departure_time time not null ,
+    arrival_date date not null ,
+    arrival_time time not null ,
+    created_date_time timestamp default current_timestamp not null,
+    modified_date_time timestamp default null on update current_timestamp,
+    version bigint not null default 0
+)
