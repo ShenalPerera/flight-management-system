eb15cd72cf4045165b172ca5cc51ce6f29b3046a
thisaramanohara <thisaramanohara@gmail.com>
2023-02-01 16:58:28 +0000

    Available service

---
+++ fms/src/main/java/com/fms/availabilitySearch/services/AvailabilitySearchService.java
@@ -0,0 +1,15 @@
+package com.fms.availabilitySearch.services;
+
+import com.fms.availabilitySearch.DAOs.AvailabilitySearchDao;
+import com.fms.availabilitySearch.models.AvailableSearch;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AvailabilitySearchService {
+    public List<AvailableSearch> getAllAvailableSearches() {
+        return null;
+//        return AvailabilitySearchDao.
+    }
+}
