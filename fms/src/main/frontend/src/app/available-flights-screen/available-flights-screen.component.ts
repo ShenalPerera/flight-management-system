65a838e26fd2b3035349777e052bf447ffe20d25
Shenal Perera <shenalperera00@gmail.com>
2023-02-02 06:17:56 +0000

    Added filtering form to the availability search screen

--- fms/src/main/frontend/src/app/available-flights-screen/available-flights-screen.component.ts
+++ fms/src/main/frontend/src/app/available-flights-screen/available-flights-screen.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AvailableFlightModel} from "./models/available-flight.model";
 import {AvailableFlightsService} from "./services/available-flights.service";
+import {AirportsHandleService} from "../services/airports-handle.service";
 
 @Component({
   selector: 'app-available-flights-screen',
@@ -10,15 +11,26 @@ import {AvailableFlightsService} from "./services/available-flights.service";
 export class AvailableFlightsScreenComponent implements OnInit{
   availableFlightList !: AvailableFlightModel[];
 
+  airports !: string[];
+
   availableFlightSearchObject = {flightNumber:"",departure:"",arrival:"",departureStartDate:"",departureEndDate:""};
-  constructor(private availableFlightService:AvailableFlightsService) {
+  constructor(private availableFlightService:AvailableFlightsService, private airportHandleService:AirportsHandleService) {
   }
   ngOnInit() {
     this.availableFlightService.getAvailableFlights(this.availableFlightSearchObject).subscribe({
       next:(filteredFlights => {
         this.availableFlightList = filteredFlights;
       }),
-      error:( error => {
+      error:( err => {
+        alert("Unexpected error occurred!");
+      })
+    });
+
+    this.airportHandleService.getAirportsList().subscribe( {
+      next:(airportsList =>{
+        this.airports = airportsList;
+      }),
+      error:(err=>{
         alert("Unexpected error occurred!");
       })
     })
